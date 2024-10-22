56469ae8c99d78ca03ed5f88b642941e4d04465a
JooeonLee <cacophony0609@gmail.com>
2024-10-22 10:22:04 +0900

    Chore(init) : 프로젝트 초기화

---
+++ src/main/java/me/jooeon/mybeauty/MybeautyApplication.java
@@ -0,0 +1,13 @@
+package me.jooeon.mybeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeautyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybeautyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jooeon/mybeauty/MybeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jooeon.mybeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeautyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
