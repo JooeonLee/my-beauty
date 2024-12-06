f9cb67f2bd5f3b1de6fd0c8535c34aebfb92d07c
JooeonLee <cacophony0609@gmail.com>
2024-12-06 14:09:32 +0900

    feat: 카테고리 관련 repository 함수 추가

--- src/main/java/me/jooeon/mybeauty/domain/cosmetic/infrastructure/CategoryRepositoryImpl.java
+++ src/main/java/me/jooeon/mybeauty/domain/cosmetic/infrastructure/CategoryRepositoryImpl.java
@@ -2,7 +2,18 @@ package me.jooeon.mybeauty.domain.cosmetic.infrastructure;
 
 import me.jooeon.mybeauty.domain.cosmetic.model.Category;
 import me.jooeon.mybeauty.domain.cosmetic.model.repository.CategoryRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepositoryImpl extends JpaRepository<Category, Long>, CategoryRepository {
+
+//    @EntityGraph(attributePaths = "children")
+//    List<Category> findByParentIdIsNull();
+
+    @EntityGraph(attributePaths = {"children", "children.children"})
+    @Query("select c from Category c where c.parent is null")
+    List<Category> findAllWithChildren();
 }
--- src/main/java/me/jooeon/mybeauty/domain/cosmetic/model/repository/CategoryRepository.java
+++ src/main/java/me/jooeon/mybeauty/domain/cosmetic/model/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package me.jooeon.mybeauty.domain.cosmetic.model.repository;
 import me.jooeon.mybeauty.domain.cosmetic.model.Category;
 import me.jooeon.mybeauty.domain.cosmetic.model.Cosmetic;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository {
@@ -12,4 +13,8 @@ public interface CategoryRepository {
     public void deleteAllInBatch();
 
     public Optional<Category> findById(Long id);
+
+    public List<Category> findAllByParentIdIsNull();
+
+    public List<Category> findAllWithChildren();
 }
