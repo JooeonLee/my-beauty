ebc4f6d87cd041000d491fd2d59ff81a5e093885
JooeonLee <cacophony0609@gmail.com>
2024-12-30 17:22:44 +0900

    feature: 인증/이간 과정에서 인증되지 않은 사용자로부터 리소스 접근 요청을 처리하기 위한 CustomAuthenticationEntryPoint 추가

---
+++ src/main/java/me/jooeon/mybeauty/global/common/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package me.jooeon.mybeauty.global.common.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.jooeon.mybeauty.global.common.model.dto.BaseResponseStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        BaseResponseStatus status = BaseResponseStatus.UNAUTHORIZED_ACCESS;
+
+        response.setContentType("application/json;charset=utf-8");
+        response.setStatus(status.getHttpStatus().value());
+        response.getWriter().write(new ObjectMapper().writeValueAsString(Map.of(
+                "isSuccess", status.isSuccess(),
+                "responseCode", status.getResponseCode(),
+                "responseMessage", status.getResponseMessage()
+        )));
+    }
+}
