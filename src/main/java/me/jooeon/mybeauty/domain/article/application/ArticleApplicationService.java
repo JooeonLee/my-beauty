ad53e9fc5193cd4f2704d2dd2523f84f3c219600
JooeonLee <cacophony0609@gmail.com>
2025-02-07 16:37:40 +0900

    feat : 다양한 도메인에 대한 정보가 필요한 기능을 처리하기 위한 ApplicationService 생성

---
+++ src/main/java/me/jooeon/mybeauty/domain/article/application/ArticleApplicationService.java
@@ -0,0 +1,35 @@
+package me.jooeon.mybeauty.domain.article.application;
+
+import me.jooeon.mybeauty.domain.article.model.dto.article.ArticleSaveRequestDto;
+import me.jooeon.mybeauty.domain.image.ImageService;
+import me.jooeon.mybeauty.domain.member.application.MemberPort;
+import me.jooeon.mybeauty.domain.member.model.Member;
+import me.jooeon.mybeauty.global.s3.model.ImagePrefix;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ArticleApplicationService {
+
+    private final ArticleService articleService;
+    private final MemberPort memberPort;
+    private final ImageService imageService;
+
+    public ArticleApplicationService(ArticleService articleService, MemberPort memberPort, ImageService imageService) {
+        this.articleService = articleService;
+        this.memberPort = memberPort;
+        this.imageService = imageService;
+    }
+
+    public long createArticle(ArticleSaveRequestDto requestDto, long memberId, MultipartFile articleImage) {
+        Member member = memberPort.getMemberById(memberId);
+
+        String articleImageUrl = null;
+        if (!articleImage.isEmpty()) {
+            articleImageUrl = imageService.upload(articleImage, ImagePrefix.ARTICLE);
+        }
+
+        return articleService.createArticle(requestDto.getTitle(), requestDto.getContent(), member, articleImageUrl);
+
+    }
+}
