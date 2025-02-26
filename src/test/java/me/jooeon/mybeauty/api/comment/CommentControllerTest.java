1ab1a1e517a256adbc8b82671cf9d5f146db9584
JooeonLee <cacophony0609@gmail.com>
2025-02-26 17:42:31 +0900

    refactor: OpenaApi3 스팩 도입에 따른comment test 코드 수정

--- src/test/java/me/jooeon/mybeauty/api/comment/CommentControllerTest.java
+++ src/test/java/me/jooeon/mybeauty/api/comment/CommentControllerTest.java
@@ -1,24 +1,31 @@
 package me.jooeon.mybeauty.api.comment;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.OpenAPI;
 import me.jooeon.mybeauty.domain.article.application.CommentApplicationService;
 import me.jooeon.mybeauty.domain.article.model.dto.article.CommentSaveRequestDto;
 import me.jooeon.mybeauty.domain.article.presentation.CommentController;
 import me.jooeon.mybeauty.global.CommonControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -48,13 +55,30 @@ public class CommentControllerTest extends CommonControllerTest {
         when(commentApplicationService.createComment(any(CommentSaveRequestDto.class), anyLong(), anyLong())).thenReturn(expectedCommentId);
 
         // when
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/app/api/articles/{articleId}/comments", testArticleId)
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.post("/app/api/articles/{articleId}/comments", testArticleId)
                 .header("Authorization", "Bearer your-bearer-token")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(requestDto)))
                 .andDo(print());
 
         // then
+//        result.andExpect(status().isOk())
+//                .andExpect(jsonPath("$.isSuccess").value(true))
+//                .andExpect(jsonPath("$.result").value(expectedCommentId))
+//                .andDo(document("comment-create",    // 문서 식별자
+//                        preprocessRequest(prettyPrint()),    // 요청 데이터 보기 좋게 출력
+//                        preprocessResponse(prettyPrint()),    // 응답 데이터 보기 좋게 출력
+//                        requestHeaders(    // 요청 헤더 문서화
+//                                headerWithName("Authorization").description("사용자 인증 및 인가를 위한 Bearer token")
+//                        ),
+//                        responseFields(
+//                                fieldWithPath("isSuccess").description("API 성공 여부"),
+//                                fieldWithPath("responseCode").description("응답 코드"),
+//                                fieldWithPath("responseMessage").description("응답 메시지"),
+//                                fieldWithPath("result").description("요청 결과 생성된 댓글 ID")
+//                        )
+//                ));
+
         result.andExpect(status().isOk())
                 .andExpect(jsonPath("$.isSuccess").value(true))
                 .andExpect(jsonPath("$.result").value(expectedCommentId))
@@ -64,11 +88,19 @@ public class CommentControllerTest extends CommonControllerTest {
                         requestHeaders(    // 요청 헤더 문서화
                                 headerWithName("Authorization").description("사용자 인증 및 인가를 위한 Bearer token")
                         ),
-                        responseFields(
-                                fieldWithPath("isSuccess").description("API 성공 여부"),
-                                fieldWithPath("responseCode").description("응답 코드"),
-                                fieldWithPath("responseMessage").description("응답 메시지"),
-                                fieldWithPath("result").description("요청 결과 생성된 댓글 ID")
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .tag("Comment API")
+                                        .summary("댓글 생성 API")
+                                        .requestHeaders(
+                                                headerWithName("Authorization").description("Bearer 토큰 (예: `Bearer your-jwt-token`)")
+                                        )
+                                        .responseFields(
+                                            fieldWithPath("isSuccess").description("API 성공 여부"),
+                                            fieldWithPath("responseCode").description("응답 코드"),
+                                            fieldWithPath("responseMessage").description("응답 메시지"),
+                                            fieldWithPath("result").description("요청 결과 생성된 댓글 ID")
+                                        ).build()
                         )
                 ));
     }
