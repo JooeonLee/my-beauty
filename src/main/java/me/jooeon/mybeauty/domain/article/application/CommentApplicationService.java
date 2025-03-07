8e442bad2cc85dfef8d188f49fa8cdda85819002
JooeonLee <cacophony0609@gmail.com>
2025-03-07 16:47:07 +0900

    feat(CommentApplicationService) : 아티클ID를 통해 댓글을 조회하고 댓글 응답 형식으로 반환하는 기능 추가

--- src/main/java/me/jooeon/mybeauty/domain/article/application/CommentApplicationService.java
+++ src/main/java/me/jooeon/mybeauty/domain/article/application/CommentApplicationService.java
@@ -1,10 +1,23 @@
 package me.jooeon.mybeauty.domain.article.application;
 
 import me.jooeon.mybeauty.domain.article.model.Article;
-import me.jooeon.mybeauty.domain.article.model.dto.article.CommentSaveRequestDto;
+import me.jooeon.mybeauty.domain.article.model.Comment;
+import me.jooeon.mybeauty.domain.article.model.dto.comment.CommentReadResponseDto;
+import me.jooeon.mybeauty.domain.article.model.dto.comment.CommentResponseDto;
+import me.jooeon.mybeauty.domain.article.model.dto.comment.CommentSaveRequestDto;
+import me.jooeon.mybeauty.domain.article.model.mapper.CommentMapper;
 import me.jooeon.mybeauty.domain.member.application.MemberPort;
 import me.jooeon.mybeauty.domain.member.model.dto.ExternalMemberDto;
+import me.jooeon.mybeauty.domain.member.model.dto.MemberSimpleProfileDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentApplicationService {
@@ -25,5 +38,23 @@ public class CommentApplicationService {
         Article article = articleService.getArticleById(articleId);
 
         return commentService.createComment(requestDto, externalMemberDto.getMemberId(), article);
+    }
+
+    public List<CommentReadResponseDto> getCommentByArticleId(long articleId) {
+
+        Article article = articleService.getArticleById(articleId);
+
+        List<Comment> commentList = commentService.findCommentByArticleId(article.getId());
+
+        Set<Long> memberIdList = commentList.stream()
+                .map(Comment::getMemberId)
+                .collect(Collectors.toSet());
+
+        Map<Long, MemberSimpleProfileDto> memberInfoMap = memberPort.getExternalMembersByIds(memberIdList).stream()
+                .collect(Collectors.toMap(ExternalMemberDto::getMemberId, MemberSimpleProfileDto::fromExternal));
+
+        return commentList.stream()
+                .map(comment -> CommentMapper.toCommentReadResponseDto(comment, memberInfoMap.get(comment.getMemberId())))
+                .toList();
     }
 }
