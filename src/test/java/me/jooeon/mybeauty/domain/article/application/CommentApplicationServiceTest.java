296fb22af2c0294d1316d7b33d7001aec0112da4
JooeonLee <cacophony0609@gmail.com>
2025-03-07 16:38:17 +0900

    test(CommentApplicationTest) : 아티클ID를 통해 댓글 조회 후 유저 정보를 포함한 댓글 응답으로 변환하여 반환하는 기능에 대한 테스트 작성

--- src/test/java/me/jooeon/mybeauty/domain/article/application/CommentApplicationServiceTest.java
+++ src/test/java/me/jooeon/mybeauty/domain/article/application/CommentApplicationServiceTest.java
@@ -1,10 +1,16 @@
 package me.jooeon.mybeauty.domain.article.application;
 
 import me.jooeon.mybeauty.domain.article.model.Article;
-import me.jooeon.mybeauty.domain.article.model.dto.article.CommentSaveRequestDto;
+import me.jooeon.mybeauty.domain.article.model.Comment;
+import me.jooeon.mybeauty.domain.article.model.dto.comment.CommentReadResponseDto;
+import me.jooeon.mybeauty.domain.article.model.dto.comment.CommentSaveRequestDto;
+import me.jooeon.mybeauty.domain.article.model.mapper.CommentMapper;
 import me.jooeon.mybeauty.domain.member.application.MemberPort;
 import me.jooeon.mybeauty.domain.member.model.dto.ExternalMemberDto;
+import me.jooeon.mybeauty.domain.member.model.dto.MemberSimpleProfileDto;
 import me.jooeon.mybeauty.fixture.ArticleFixture;
+import me.jooeon.mybeauty.fixture.CommentFixture;
+import me.jooeon.mybeauty.fixture.ExternalMemberDtoFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +18,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(SpringExtension.class)
@@ -61,6 +74,45 @@ public class CommentApplicationServiceTest {
 
         // then
         assertThat(actualCommentId).isEqualTo(expectedCommentId);
-
     }
+
+    @DisplayName("아티클ID를_통해_댓글_조회에_성공하면_댓글_응답형식으로_변환하여_반환한다")
+    @Test
+    void getCommentByArticleId() {
+
+        // given
+        Article testArticle = ArticleFixture.아티클_아이디포함(1L, "테스트_아티클_제목", "테스트_아티클_내용");
+
+        List<Comment> comments = Arrays.asList(
+                CommentFixture.댓글_아이디_생성시간_포함(1L, testArticle, "테스트_아티클_내용1", 2L),
+                CommentFixture.댓글_아이디_생성시간_포함(2L, testArticle, "테스트_아티클_내용2", 3L),
+                CommentFixture.댓글_아이디_생성시간_포함(3L, testArticle, "테스트_아티클_내용3", 4L),
+                CommentFixture.댓글_아이디_생성시간_포함(4L, testArticle, "테스트_아티클_내용4", 5L)
+        );
+
+        List<ExternalMemberDto> externalMembers = Arrays.asList(
+                ExternalMemberDtoFixture.외부이용멤버(2L),
+                ExternalMemberDtoFixture.외부이용멤버(3L),
+                ExternalMemberDtoFixture.외부이용멤버(4L),
+                ExternalMemberDtoFixture.외부이용멤버(5L)
+        );
+
+        Map<Long, MemberSimpleProfileDto> memberInfoMap = externalMembers.stream()
+                .collect(Collectors.toMap(ExternalMemberDto::getMemberId, MemberSimpleProfileDto::fromExternal));
+
+        List<CommentReadResponseDto> commentReadResponseDtos = comments.stream()
+                .map(comment -> CommentMapper.toCommentReadResponseDto(comment, memberInfoMap.get(comment.getMemberId())))
+                .toList();
+
+        when(articleService.getArticleById(testArticle.getId())).thenReturn(testArticle);
+        when(commentService.findCommentByArticleId(testArticle.getId())).thenReturn(comments);
+        when(memberPort.getExternalMembersByIds(any())).thenReturn(externalMembers);
+
+        // when
+        List<CommentReadResponseDto> result = commentApplicationService.getCommentByArticleId(testArticle.getId());
+
+        // then
+        assertThat(result.size()).isEqualTo(commentReadResponseDtos.size());
+    }
+
 }
--- src/test/java/me/jooeon/mybeauty/fixture/CommentFixture.java
+++ src/test/java/me/jooeon/mybeauty/fixture/CommentFixture.java
@@ -5,6 +5,7 @@ import me.jooeon.mybeauty.domain.article.model.Comment;
 import me.jooeon.mybeauty.global.common.model.enums.Status;
 
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 
 public class CommentFixture {
 
@@ -27,6 +28,13 @@ public class CommentFixture {
         return comment;
     }
 
+    public static Comment 댓글_아이디_생성시간_포함(long commentId, Article article, String content, long memberId) {
+        Comment comment = 댓글(article, content, memberId);
+        setId(comment, commentId);
+        setCreatedAt(comment);
+        return comment;
+    }
+
     // Reflection 을 이용하여 ID 값을 강제 설정
     private static void setId(Comment comment, long id) {
         try {
@@ -35,6 +43,17 @@ public class CommentFixture {
             field.set(comment, id);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException("Failed to set id of comment", e);
+        }
+    }
+
+    // Reflection 을 이용하여 생성 시간을 강제 설정
+    private static void setCreatedAt(Comment comment) {
+        try {
+            Field field = Comment.class.getSuperclass().getDeclaredField("createdAt");
+            field.setAccessible(true);
+            field.set(comment, LocalDateTime.now());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("Failed to set createdAt of comment", e);
         }
     }
 }
