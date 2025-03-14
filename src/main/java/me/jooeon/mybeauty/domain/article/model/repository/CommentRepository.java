cd86a39f7d8ee084b9a08226f0d6109cabbeca17
JooeonLee <cacophony0609@gmail.com>
2025-03-14 11:22:36 +0900

    feat(CommentService) : 댓글ID를 통한 댓글 조회 기능 추가

--- src/main/java/me/jooeon/mybeauty/domain/article/model/repository/CommentRepository.java
+++ src/main/java/me/jooeon/mybeauty/domain/article/model/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package me.jooeon.mybeauty.domain.article.model.repository;
 import me.jooeon.mybeauty.domain.article.model.Comment;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository {
 
@@ -11,4 +12,6 @@ public interface CommentRepository {
     public List<Comment> findByArticleId(long articleId);
 
     public long countByArticleId(long articleId);
+
+    public Optional<Comment> findById(long commentId);
 }
