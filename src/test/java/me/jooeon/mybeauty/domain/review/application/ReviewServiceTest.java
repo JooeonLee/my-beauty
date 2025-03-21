f237990703580a54a92a0d880eff8f95d1563dbb
JooeonLee <cacophony0609@gmail.com>
2025-03-21 14:54:14 +0900

    hotfix : 테스트 프로파일 문제 해결

--- src/test/java/me/jooeon/mybeauty/domain/review/application/ReviewServiceTest.java
+++ src/test/java/me/jooeon/mybeauty/domain/review/application/ReviewServiceTest.java
@@ -40,7 +40,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@ActiveProfiles({"test", "secret"})
+@ActiveProfiles("test")
 @SpringBootTest
 public class ReviewServiceTest {
 
